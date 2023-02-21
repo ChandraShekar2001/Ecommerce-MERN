a3b203494e710184742c2d25b5642a42a1ba12f0
ChandraShekar2001 <pvchandu07@gmail.com>
2023-02-21 17:27:34 +0530

    Initial commit

---
+++ backend/config/database.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors())
+
+ mongoose.set('strictQuery', false)
+
+const connectDatabase = () => {
+  mongoose
+    .connect('mongodb://localhost:27017/Ecommerce', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((data) => {
+      console.log(`Mongodb connected with server: ${data.connection.host}`);
+    });
+};
+
+module.exports = connectDatabase;
